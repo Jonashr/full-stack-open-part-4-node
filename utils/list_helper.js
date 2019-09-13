6c3396f0091490912d412f113122feeac67133aa
Jonas Horvei <jonashorvei1@gmail.com>
2019-09-13 06:13:13 +0200

    Helper function + tests for summing up the numer of likes for all blog posts.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let sum = blogs.reduce((accumulated, blog) => {
+        return accumulated + blog.likes
+    }, 0)
+
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
